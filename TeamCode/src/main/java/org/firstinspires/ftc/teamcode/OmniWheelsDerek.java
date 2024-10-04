2a6b6af4c114f3ae2846348eb2855136577a65b3
derekz09 <derek@thezakarians.com>
2024-10-04 10:16:52 -0700

    Update OmniWheelsDerek.java
    Fixed more problems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniWheelsDerek.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniWheelsDerek.java
@@ -237,11 +237,11 @@ public class OmniWheelsDerek extends LinearOpMode {
 
             // MotorSpeed: speed of each motor.
             // ServoPosition position of each servo
-            for (DcMotor thisMotor; allMotors) {
+            for (DcMotor thisMotor in allMotors) {
                 telemetry.addData("MotorSpeed", thismotor.getSpeed());
             }
-
-            for (Servo thisServo; allServos) {
+            
+            for (Servo thisServo in allServos) {
                 telemetry.addData("ServoPosition", thisServo.getPosition());
             }
             telemetry.update();
